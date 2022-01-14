e95b2361f6ec7142ae308ed89af33dc58c39b0b8
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2022-01-14 09:29:08 +0100

    'no-inferrable-types': 'off' // explizite Deklarationen wie let x : number = 1; erlauben!

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-inferrable-types': 'off' // explizite Deklarationen wie let x : number = 1; erlauben!
   },
   overrides: [
     {
