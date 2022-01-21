ced63fac502d4f14aa019ee41ad4a10f005e661d
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2022-01-21 12:59:54 +0100

    +npm babel-jest

--- tests/unit/Login.spec.ts
+++ tests/unit/Login.spec.ts
@@ -24,33 +24,39 @@ describe("Login test", () => {
   const wrapper = mount(Login, {
    global: {
     provide: {
+     // wird immer false liefern bei Login
      AuthenticationManager: new AuthenticationManagerMock(false),
     },
     mocks: {},
    },
   });
-
+  // Prüfe Überschrift
   expect(wrapper.get(".panel-heading").text()).toEqual("User Login");
+  // Loginformular ausfüllen
   await wrapper.find("input[name=username]").setValue("unittest");
-  await wrapper.find("input[name=password]").setValue("unsinn");
+  await wrapper.find("input[name=password]").setValue("falschesKennwort");
   await wrapper.find("button[type=submit]").trigger("click");
+  // Prüfe Ergebnis
   expect(wrapper.get("#errorMsg").text()).toEqual("Login Error!");
  });
 
  it("Login OK", async () => {
-
   const wrapper = mount(Login, {
    global: {
     provide: {
+     // wird immer true liefern bei Login
      AuthenticationManager: new AuthenticationManagerMock(true),
     },
     mocks: {},
    },
   });
 
+  // Prüfe Überschrift
   expect(wrapper.get(".panel-heading").text()).toEqual("User Login");
+  // Loginformular ausfüllen
   await wrapper.find("input[name=username]").setValue("unittest");
-  await wrapper.find("input[name=password]").setValue("unsinn");
+  await wrapper.find("input[name=password]").setValue("richtigesKennwort");
+  // Prüfe Ergebnis
   await wrapper.find("button[type=submit]").trigger("click");
  });
 });
