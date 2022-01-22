66f9266b8f1f74aa32b2b92f1b69ddb09894d281
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2022-01-22 16:46:18 +0100

    Test erweitert

--- tests/e2e/specs/Diagnose.js
+++ tests/e2e/specs/Diagnose.js
@@ -4,14 +4,19 @@
 require("chai").expect;
 
 module.exports = {
- "diagnose": (browser) => {
+ diagnose: (browser) => {
   browser.init();
 
-  browser.url(function(result) {
-   // return the current url
+  browser.url(function (result) {
+   // print the current url
    console.log(result);
- });
- 
- },
+  });
 
+  // classic Nightwatch Assertion
+  browser.assert.urlContains("/");
+  // Assertion with Chai
+  browser.expect.element('body').to.be.present;
+
+  browser.end();
+ },
 };
