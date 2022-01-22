fc54a8ea24e4a497c034ce8f33a0c7f241177f0e
Dr. Holger Schwichtenberg <hs@IT-Visions.de>
2022-01-22 13:46:54 +0100

    This plugin removes all console.* calls.

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+// This plugin removes all console.* calls.
+// https://www.npmjs.com/package/babel-plugin-transform-remove-consolenp
+const removeConsolePlugin = []
+if(process.env.NODE_ENV === 'production') {
+  removeConsolePlugin.push("transform-remove-console")
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins: removeConsolePlugin
 }
